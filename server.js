07a86e1dd895f000fa634c94d49191838c7fa9fa
Worasit Daimongkol <worasit.dai@ascendcorp.com>
2019-12-26 21:24:08 +0700

    Added assignment 1

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.listen(port);
+console.log(`Server started at port: ${port}`);
