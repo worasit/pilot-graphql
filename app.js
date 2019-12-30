78692bf1cd567a745bf2c2ee8b396e0406d79939
Worasit Daimongkol <worasit.dai@ascendcorp.com>
2019-12-30 19:18:02 +0700

    Added rootDir utils and applied to the code.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const rootDir = require('./utils/path');
 
 const app = express();
 const port = 3000;
@@ -14,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(port);
