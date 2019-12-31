d7dcce376e5f21653c9b126f43ad9b7aabcfda9d
Worasit Daimongkol <worasit.dai@ascendcorp.com>
2019-12-31 15:39:31 +0700

    Added styles.

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const rootDir = require('../utils/path');
 
 router.get('/add-product', (req, res, next) => {
-    const htmlTemplate = /*html*/
-        `
-        <form action="/admin/product" method="POST">
-            <input type="text" name="title">
-            <button type="submit">Add Product</button>
-        </form>
-        `;
-    res.send(htmlTemplate);
+    res.sendfile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
