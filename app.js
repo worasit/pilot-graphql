849c5e73b837aa0aacada1c2f7b1b25edf5393b0
worasit daimongkol <worasit.daimongkol@agoda.com>
2020-01-08 13:32:03 +0700

    Initialized project to support templating engine implemenation.

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const rootDir = require('./utils/path');
 
 const app = express();
-const port = 3000;
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(port);
-console.log(`Service is running at port: ${port}`);
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,15 @@
 const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
-const rootDir = require('../utils/path');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log('shop.js', adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
